14f062f75d3c3a7beea2525bcf5cbfa57832148f
Nouran Samy <nourans.96@yahoo.com>
2021-08-27 21:12:15 +0200

    Initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/" class="pe-3">Movies</router-link>
+    <router-link to="/watch-list" class="pe-3">Watch list</router-link>
   </div>
   <router-view />
 </template>
 
 <style lang="scss">
+@import "./scss/base";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
 #nav {
@@ -21,9 +21,21 @@
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
+    position: relative;
 
     &.router-link-exact-active {
-      color: #42b983;
+      color: $primaryColor;
+
+      &::after {
+        content: "";
+        position: absolute;
+        width: 50%;
+        height: 2px;
+        background: $primaryColor;
+        top: 1.3rem;
+        left: 0;
+      }
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,13 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/watch-list",
+    name: "Watch list",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "watchList" */ "../views/WatchList.vue"),
   },
 ];
 
