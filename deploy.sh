317886ed8556b74bdf3c390601e2974b863b45d2
Nouran Samy <nourans.96@yahoo.com>
2021-10-22 21:43:09 +0200

    Change repo name

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:nouran96/Movies-app-vue.git main:gh-pages
+git push -f git@github.com:Nouran96/Movies-app-vue.git main:gh-pages
 cd -
